b2f40381f76eda64483946ec68ae5c04d942560b
jessejp <jessepernila@gmail.com>
2023-11-02 17:20:38 +0200

    setup: express middleware

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,6 +2,9 @@ import { MikroORM } from "@mikro-orm/core";
 import type { PostgreSqlDriver } from "@mikro-orm/postgresql";
 import MikroORMConfig from "./mikro-orm.config";
 import { ApolloServer } from "@apollo/server";
+import { expressMiddleware } from "@apollo/server/express4";
+import cors from "cors";
+import express from "express";
 
 const typeDefs = `#graphql
   type Query {
@@ -16,13 +19,29 @@ const resolvers = {
 };
 
 const main = async () => {
+  // Database connection
   const orm = await MikroORM.init<PostgreSqlDriver>(MikroORMConfig);
   await orm.getMigrator().up();
+
+  const app = express();
 
   const server = new ApolloServer({
     typeDefs,
     resolvers,
   });
+
+  await server.start();
+
+  app.use(
+    "/graphql",
+    cors<cors.CorsRequest>(),
+    express.json(),
+    expressMiddleware(server)
+  );
+
+  // Modified server startup
+  await new Promise<void>((resolve) => app.listen({ port: 4000 }, resolve));
+  console.log(`🚀 Server ready at http://localhost:4000/`);
 };
 
 main().catch((err) => {
